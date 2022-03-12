b4805191e86282d9a520e6e2f5b9b287704069d2
enricopic85 <enricodev85@gmail.com>
2022-03-12 16:43:11 +0100

    creazione componente App.vue

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     public function index()
     {
+        $user= Auth::user();
         return view('admin.home');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
-Route::get('/admin', 'Admin\HomeController@index')->name('admin.home');
+
+
+Route::middleware("auth")
+    ->namespace("Admin")
+    ->prefix("admin")
+    ->name("admin.")
+    ->group(function(){
+        Route::get('/', 'HomeController@index')->name('home');
+        });
+
+Route::get("{any?}",function(){
+    return view("guest.home");
+})->where("any",".*");
